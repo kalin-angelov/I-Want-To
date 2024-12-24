c9651f30e9fbfecba98cd55d696dc8fb0d092bc5
Kalin Angelov <kalin.st.angelov@gmail.com>
2024-12-24 11:15:10 +0200

    Update HomePage.jsx

--- frontend/src/components/custom/HomePage.jsx
+++ frontend/src/components/custom/HomePage.jsx
@@ -1,11 +1,22 @@
+import { Container, Heading } from '@chakra-ui/react';
 
+import WhishList from './WhishList';
 
 const HomePage = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Container>
+        <Heading 
+            as={"h1"} 
+            size={{ base: "2xl", sm: "3xl" }} 
+            textAlign={"center"} 
+            m={8}
+        >
+            My whish list
+        </Heading>
+        
+        <WhishList />   
+    </Container>
+  );
+};
 
-export default HomePage
+export default HomePage;
