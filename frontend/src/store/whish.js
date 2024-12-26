3ff58393ad1f7da9173d2b540f9b4e66754d2b7a
Kalin Angelov <kalin.st.angelov@gmail.com>
2024-12-26 18:22:51 +0200

    Update whish.js

--- frontend/src/store/whish.js
+++ frontend/src/store/whish.js
@@ -5,11 +5,11 @@ export const useWhishStore = create ((set) => ({
     setWhishes: (whishes) => set( {whishes} ),
     createWhish: async(newWhish) => {
 
-        if(!newWhish.type || !newWhish.brand || !newWhish.model) {
-            return { success: false, message: "Please provide all fields."};
+        if(!newWhish.whish) {
+            return { success: false, message: "Please don't make empty whishes."};
         }
 
-        const response = await fetch(`api/vehicle`, {
+        const response = await fetch(`api/whish`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json" 
@@ -22,8 +22,17 @@ export const useWhishStore = create ((set) => ({
         return { success: true, message: "Whish added successfully."};
     },
     fetchWhishes: async() => {
-        const response = await fetch(`api/vehicles`);
+        const response = await fetch(`api/whishes`);
         const data = await response.json();
         set({ whishes: data.data});
+    },
+    deleteWhish: async(id) => {
+        const response = await fetch(`api/whish/${id}`, {
+            method: "DELETE"
+        });
+        const data = await response.json();
+        if (!data.success) return { success: false, message: data.message };
+        set(state => ({ whishes: state.whishes.filter(whish => whish._id !== id)}) );
+        return { success: true, message: data.message };
     }
 }));
