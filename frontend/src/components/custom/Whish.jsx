1c1d8db26bf9f34b0f46bcb7c7dba47d960ee234
Kalin Angelov <kalin.st.angelov@gmail.com>
2024-12-26 18:22:49 +0200

    Update Whish.jsx

--- frontend/src/components/custom/Whish.jsx
+++ frontend/src/components/custom/Whish.jsx
@@ -1,8 +1,30 @@
-import { Button, Card, CardBody, Flex, HStack, Text } from '@chakra-ui/react';
+import { Card, CardBody, Flex, HStack, IconButton, Text } from '@chakra-ui/react';
 import { FaRegTrashAlt, FaRegEdit  } from 'react-icons/fa';
 import { useColorModeValue } from '../ui/color-mode';
+import { useWhishStore } from '@/store/whish';
+import { toaster } from '@/components/ui/toaster';
 
 const Whish = ( { whish }) => {
+
+    const { deleteWhish } = useWhishStore();
+
+    const onDelete = async (id) => {
+        const { success, message } = await deleteWhish(id);
+
+        if (!success) {
+            toaster.error({
+                title: "Error",
+                description: message,
+            })
+        } else {
+            toaster.success({
+                title: "Success",
+                description: message,
+            })
+        }
+          
+    };
+
   return (
     <Card.Root w={"full"} bg={useColorModeValue("gray.100", "gray.700")}>
             <CardBody >
@@ -12,20 +34,16 @@ const Whish = ( { whish }) => {
                     justifyContent={"space-between"}
                     gap={4}
                 >
-                    <Text>I want to drive {whish.brand} {whish.model}, it's a type of a {whish.type}.</Text>
+                    <Text>{whish.whish}</Text>
 
                     <HStack>
-                        <Button 
-                            bg={"blue.300"}
-                        >
+                        <IconButton bg={"blue.300"}>
                             <FaRegEdit />
-                        </Button>
+                        </IconButton>
 
-                        <Button 
-                            bg={"red.500"}
-                        >
+                        <IconButton bg={"red.500"} onClick={() => onDelete(whish._id)}>
                             <FaRegTrashAlt />
-                        </Button>
+                        </IconButton>
                     </HStack>
                 </Flex>
             </CardBody>
