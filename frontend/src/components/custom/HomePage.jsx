c71b959a46970260b48eeba70761b341558d8a5e
Kalin Angelov <kalin.st.angelov@gmail.com>
2024-12-26 18:22:47 +0200

    Update HomePage.jsx

--- frontend/src/components/custom/HomePage.jsx
+++ frontend/src/components/custom/HomePage.jsx
@@ -1,31 +1,43 @@
-import { Container, Heading } from '@chakra-ui/react';
+import { Container, Heading, Span, Text } from '@chakra-ui/react';
 import { useWhishStore } from '@/store/whish';
 import { useEffect } from 'react';
 
 import WhishList from './WhishList';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
 
-  const { fetchWhishes } = useWhishStore();
-  
-  useEffect(() => {
-    fetchWhishes();
-  },[fetchWhishes]);
+    const { fetchWhishes, whishes } = useWhishStore();
 
-  return (
-    <Container>
-        <Heading 
-            as={"h1"} 
-            size={{ base: "2xl", sm: "3xl" }} 
-            textAlign={"center"} 
-            m={8}
-        >
-            My whish list
-        </Heading>
-        
-        <WhishList />   
-    </Container>
-  );
+    useEffect(() => {
+        fetchWhishes();
+    },[fetchWhishes]);
+
+    return (
+        <Container>
+            <Heading 
+                as={"h1"} 
+                size={{ base: "2xl", sm: "3xl" }} 
+                textAlign={"center"} 
+                m={8}
+            >
+                My whish list
+            </Heading>
+            
+            {whishes && <WhishList />}
+
+            {whishes.length == 0 &&
+                <Text textAlign={"center"}>
+                    Whish list is empty. 
+                    <Link to={"/create"}>
+                        <Text as={"span"} p={1} color={"blue.400"}>
+                            Make a whish🌠
+                        </Text>
+                    </Link>
+                </Text>
+            }
+        </Container>
+    );
 };
 
 export default HomePage;
