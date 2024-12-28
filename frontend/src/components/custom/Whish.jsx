cbfda6c672bac957e7008b4277f6591d074de75e
Kalin Angelov <kalin.st.angelov@gmail.com>
2024-12-28 12:24:23 +0200

    Update Whish.jsx

--- frontend/src/components/custom/Whish.jsx
+++ frontend/src/components/custom/Whish.jsx
@@ -4,9 +4,17 @@ import { useColorModeValue } from '../ui/color-mode';
 import { useWhishStore } from '@/store/whish';
 import { toaster } from '@/components/ui/toaster';
 
+import EditWhish from './EditWhish';
+import { useState } from 'react';
+
 const Whish = ( { whish }) => {
 
     const { deleteWhish } = useWhishStore();
+    const [open, setOpen] = useState(false);
+
+    const openEditModal = () => {
+        setOpen(true);
+    };
 
     const onDelete = async (id) => {
         const { success, message } = await deleteWhish(id);
@@ -27,26 +35,28 @@ const Whish = ( { whish }) => {
 
   return (
     <Card.Root w={"full"} bg={useColorModeValue("gray.100", "gray.700")}>
-            <CardBody >
-                <Flex
-                    flexDir={{ base: "column", sm: "row" }}
-                    alignItems={"center"}
-                    justifyContent={"space-between"}
-                    gap={4}
-                >
-                    <Text>{whish.whish}</Text>
+        <CardBody >
+            <Flex
+                flexDir={{ base: "column", sm: "row" }}
+                alignItems={"center"}
+                justifyContent={"space-between"}
+                gap={4}
+            >
+                <Text>{whish.whish}</Text>
 
-                    <HStack>
-                        <IconButton bg={"blue.300"}>
-                            <FaRegEdit />
-                        </IconButton>
+                <HStack>
+                    <IconButton bg={"blue.300"} onClick={openEditModal}>
+                        <FaRegEdit />
+                    </IconButton>
 
-                        <IconButton bg={"red.500"} onClick={() => onDelete(whish._id)}>
-                            <FaRegTrashAlt />
-                        </IconButton>
-                    </HStack>
-                </Flex>
-            </CardBody>
+                    <IconButton bg={"red.500"} onClick={() => onDelete(whish._id)}>
+                        <FaRegTrashAlt />
+                    </IconButton>
+                </HStack>
+            </Flex>
+        </CardBody>
+
+        <EditWhish open={open} setOpen={setOpen} whish={whish} />
     </Card.Root>
   );
 };
