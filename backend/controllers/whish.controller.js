4a3597906c528c70a96a107f6f0132f5d779c2ee
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-01-03 16:57:29 +0200

    Update whish.controller.js

--- backend/controllers/whish.controller.js
+++ backend/controllers/whish.controller.js
@@ -1,13 +1,14 @@
-import mongoose from 'mongoose';
 import Whish from '../models/whish.model.js';
+
+import { createWhishManager, updateWhishManager, deleteWhishManager } from '../manager/whish.manager.js';
 
 export const getWhishes = async (req, res) => {
     try {
         const whishes = await Whish.find({});
-        res.status(200).json({ success: true, data: whishes});
+        res.status(200).json({ status: 200, success: true, data: whishes});
     } catch (error) {
         console.log(`Error message: ${error.message}`);
-        res.status(500).json({ success: false ,message: error.message });
+        res.status(500).json({ status: 500, success: false ,message: error.message });
     }
 };
 
@@ -15,16 +16,12 @@ export const createWhish = async(req, res) => {
 
     const whish = req.body
 
-    if(!whish.whish) {
-        return res.status(400).json({ success: false, message: "Please provide a whish"});
-    }
-
     try {
-        const newWhish = await Whish.create(whish);
-        res.status(201).json({ success: true, data: newWhish });
+        const newWhish = await createWhishManager(whish);
+        res.status(newWhish.status).json({ status: newWhish.status, success: true, data: newWhish });
     } catch (error) {
         console.log(`Error message: ${error.message}`);
-        res.status(500).json({ success: false, message: error.message});
+        res.status(error.status).json({ status: error.status, success: false, message: error.message});
     }
 };
 
@@ -33,16 +30,12 @@ export const updateWhish = async(req, res) => {
     const { id } = req.params;
     const whish = req.body;
 
-    if(!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ success: false, message: "Invalid whish id" });
-    }
-
     try {
-        const updatedWhish = await Whish.findByIdAndUpdate(id, whish, { new: true });
-        res.status(201).json({ success: true, data: updatedWhish});
+        const updatedWhish = await updateWhishManager(id, whish);
+        res.status(updatedWhish.status).json({ status: updateWhish.status, success: true, data: updatedWhish.data});
     } catch (error) {
         console.log(`Error message: ${error.message}`);
-        res.status(500).json({ success: false, message: error.message });
+        res.status(error.status).json({ status: error.status, success: false, message: error.message });
     }
 };
 
@@ -51,10 +44,10 @@ export const deleteWhish = async (req,res) => {
     const { id } = req.params;
 
     try {
-        await Whish.findByIdAndDelete(id);
-        res.status(200).json({ success: true, message: "Whish deleted successfully" });
+        const deletedWhish = await deleteWhishManager(id);
+        res.status(deletedWhish.status).json({ status: deleteWhish.status, success: true, message: deletedWhish.message });
     } catch (error) {
         console.log(`Error message: ${error.message}`);
-        res.status(500).json({ success: false, message: error.message });
+        res.status(error.status).json({ status: error.status, success: false, message: error.message });
     }
 };
